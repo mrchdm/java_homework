7f874b1338e16ace3c80d34e370fb156810910b0
mrchdm <dementevamm@yandex.ru>
2023-01-20 15:20:04 +0300

    Remove mistakes

---
+++ Book.java
@@ -0,0 +1,27 @@
+package java_homework;
+
+class Book {
+    String title;
+    String author;
+
+    public Book(String title, String author) {
+        this.title = title;
+        this.author = author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                '}';
+    }
+}
